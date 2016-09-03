b79c59f0a791bc2eafcc2ec5f519708eaf7ecdd7
Camila Sabino <camila2sabino@gmail.com>
2016-09-02 21:17:23 -0300

    Pre merge.

--- pois-ui-angular/js/app.js
+++ pois-ui-angular/js/app.js
@@ -1,2 +1,43 @@
 'use strict';
-angular.module('poisApp', [ ])
+
+var app = angular.module('poisApp', [ ]);
+
+/* Controllers */
+app.controller('buscadorCtrl', function () {
+	/* scope */
+	this.resultados = []; // or null
+	this.poiSeleccionado = null;
+	this.nuevoCriterio = '';
+	this.criteriosBusqueda = [];
+	this.initStatus = false;
+	this.mensajeInvalido = '';
+	this.criterioSeleccionado = '';
+	/*-----------------------------------------------------------------------------------*/
+	this.repo = null;
+	this.busquedas = [];
+	//this.usuarioActual = null;
+	//this.fechaActual = new Date();
+	
+	this.buscar = function(texto){
+		
+		function checkearQuery(texto, poi) {
+			texto !== null && (poi.tienePalabraClave(texto) || poi.coincide(texto))
+		}
+		
+		this.repo.filter(checkearQuery.bind(this, checkearQuery))
+	}
+
+	this.agregarCriterio = function(){
+
+		if (this.nuevoCriterio !== ''){
+			this.criteriosBusqueda.push(nuevoCriterio)
+			this.nuevoCriterio = ''
+		}
+		// else ...
+	}
+
+	this.eliminarCriterio = function(){
+		this.criteriosBusqueda.pop(this.criterioSeleccionado)
+	}
+
+});
