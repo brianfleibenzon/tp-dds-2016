605b33e6695bb453b665eac962de0f9ac15b56e8
Camila Sabino <camila2sabino@gmail.com>
2016-09-06 21:08:51 -0300

    Pre merge.

--- pois-ui-angular/app/modules/login/controllers/loginCtrl.js
+++ pois-ui-angular/app/modules/login/controllers/loginCtrl.js
@@ -1,43 +1,43 @@
 function LoginCtrl($state) {
-	var self = this;
-	
-	var usuarioIdActual = 1;
+    var self = this;
 
-	function Usuario (usuario, clave) {
-		return { id: usuarioIdActual++, usuario: usuario, clave: clave};
-	}
-	
-	self.usuarios = [
-		new Usuario("juanPerez", "1234")
-	];
-	
-	var usuario;
-	var clave;
-	var resultadoLogin;
-	
-	var resultadoLogin = "";
-		
-	self.login = function () {
-		var usuarioEncontrado = _.find(self.usuarios, { usuario: this.usuario });
-		if (usuarioEncontrado){
-			if (usuarioEncontrado.clave == this.clave){
-				$state.go("main.busqueda_pois");
-			}else{
-				this.clave = "";
-				this.resultadoLogin = "La clave ingresada es incorrecta (intente con 1234).";
-				$state.go("main.login.incorrecto");
-			}
-		}else{
-			this.usuario = "";
-			this.clave = "";
-			this.resultadoLogin = "El usuario ingresado no existe (intente con juanPerez).";
-			$state.go("main.login.incorrecto");
-		}
-	};
+    var usuarioIdActual = 1;
+
+    function Usuario(usuario, clave) {
+        return { id: usuarioIdActual++, usuario: usuario, clave: clave };
+    }
+
+    self.usuarios = [
+        new Usuario("juanPerez", "1234")
+    ];
+
+    var usuario;
+    var clave;
+    var resultadoLogin;
+
+    var resultadoLogin = "";
+
+    self.login = function() {
+        var usuarioEncontrado = _.find(self.usuarios, { usuario: this.usuario });
+        if (usuarioEncontrado) {
+            if (usuarioEncontrado.clave === this.clave) {
+                $state.go("main.busqueda_pois");
+            } else {
+                this.clave = "";
+                this.resultadoLogin = "La clave ingresada es incorrecta (intente con '1234').";
+                $state.go("main.login.incorrecto");
+            }
+        } else {
+            this.usuario = "";
+            this.clave = "";
+            this.resultadoLogin = "El usuario ingresado no existe (intente con 'juanPerez').";
+            $state.go("main.login.incorrecto");
+        }
+    };
 
 };
 
 angular.module('pois-app')
-.controller('LoginCtrl', LoginCtrl);
+    .controller('LoginCtrl', LoginCtrl);
 
 LoginCtrl.$inject = ["$state"];
--- pois-ui-angular/app/modules/pois/controllers/busquedaPoisCtrl.js
+++ pois-ui-angular/app/modules/pois/controllers/busquedaPoisCtrl.js
@@ -1,50 +1,50 @@
 function BusquedaPoisCtrl(pois, PoisHome) {
-	var self = this;
-	self.pois = pois;
-	self.nuevoCriterio = ""; // String
-	self.criteriosDeBusqueda = []; // Lista de String
-	self.criterioSeleccionado = "";
-  
-	// FUNCIÓN 'BUSCAR':
-	self.buscar = function() {
-		
-		self.poisBusqueda = [];
-		
-		_(self.pois).forEach(function(poi) {
-			var self2 = this;
-			self2.poi = poi;
-			_(self.criteriosDeBusqueda).forEach(function(criterio) {
-				if (_.includes(self2.poi.nombre, criterio) || _.includes(self2.poi.palabrasClave, criterio)){
-					self.poisBusqueda.push(self2.poi);
-				}
-			});
-		});
-		
-		
-		
-	};
-	
-	self.agregarCriterio = function() {
-		if(self.nuevoCriterio != "" &&  !(_.includes(self.criteriosDeBusqueda, self.nuevoCriterio))){			
-			self.criteriosDeBusqueda.push(self.nuevoCriterio);			
-		}
-		self.nuevoCriterio = "";
-	};
-	
-	self.quitarCriterio = function() {
-		function remove(arr, item) {
-			 for(var i = arr.length; i--;) {
-				 if(arr[i] === item) {
-					 arr.splice(i, 1);
-				 }
-			 }
-		}
-		remove(self.criteriosDeBusqueda, self.criterioSeleccionado);
-		self.criterioSeleccionado = "";
-	};
+    var self = this;
+    self.pois = pois;
+    self.nuevoCriterio = ""; // String
+    self.criteriosDeBusqueda = []; // Lista de String
+    self.criterioSeleccionado = "";
+    var estadoResultado = "";
+
+    // FUNCIÓN 'BUSCAR':
+    self.buscar = function() {
+
+        if (self.criteriosDeBusqueda.length === 0) self.estadoResultado = "No ha seleccionado ningún criterio de búsqueda.";
+        self.poisBusqueda = [];
+
+        _(self.pois).forEach(function(poi) {
+            var self2 = this;
+            self2.poi = poi;
+            _(self.criteriosDeBusqueda).forEach(function(criterio) {
+                if (_.includes(self2.poi.nombre, criterio) || _.includes(self2.poi.palabrasClave, criterio)) {
+                    self.poisBusqueda.push(self2.poi);
+                }
+            });
+        });
+		 if (self.poisBusqueda.length === 0) self.estadoResultado = "No se han encontrado resultados para su búsqueda.";
+    };
+
+    self.agregarCriterio = function() {
+        if (self.nuevoCriterio != "" && !(_.includes(self.criteriosDeBusqueda, self.nuevoCriterio))) {
+            self.criteriosDeBusqueda.push(self.nuevoCriterio);
+        }
+        self.nuevoCriterio = "";
+    };
+
+    self.quitarCriterio = function() {
+        function remove(arr, item) {
+            for (var i = arr.length; i--;) {
+                if (arr[i] === item) {
+                    arr.splice(i, 1);
+                }
+            }
+        }
+        remove(self.criteriosDeBusqueda, self.criterioSeleccionado);
+        self.criterioSeleccionado = "";
+    };
 }
 
 angular.module("pois-app")
-.controller("BusquedaPoisCtrl", BusquedaPoisCtrl);
+    .controller("BusquedaPoisCtrl", BusquedaPoisCtrl);
 
-BusquedaPoisCtrl.$inject = [ "pois", "PoisHome" ];
+BusquedaPoisCtrl.$inject = ["pois", "PoisHome"];
